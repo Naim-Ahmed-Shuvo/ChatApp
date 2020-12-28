e7deb77c63105bb03eccd5f6e3abb7c1501cd100
shuvo <shuvonaim123@gmail.com>
2020-12-28 14:13:47 +0600

    initial commit

---
+++ resources/js/Pages/Chat/messageItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">
+      {{message.user.name}} : {{message.message}}
+  </div>
+</template>
+
+<script>
+export default {
+   props: ['message']
+}
+</script>
+
+<style>
+
+</style>
